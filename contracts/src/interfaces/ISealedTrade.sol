1b7dc3ed12a8d528ffcccde51d4285228ffce702
Shota Nagafuchi <ShotaNagafuchi@users.noreply.github.com>
2026-05-13 00:52:05 +0900

    fix: sync interface with concrete contracts
    - TradeNegotiating: add attestationHash parameter
    - TradeAgreed: add finalDealValue, attestationHash (was missing 2 params)
    - IBondVault: add escalateBond and getBondAmount (were missing)

--- contracts/src/interfaces/ISealedTrade.sol
+++ contracts/src/interfaces/ISealedTrade.sol
@@ -13,8 +13,8 @@ interface ISealedTrade {
 
     event TradeListed(bytes32 indexed tradeId, address indexed seller, bytes32 assetHash, uint256 maxDealValue);
     event TradeMatched(bytes32 indexed tradeId, address indexed buyer);
-    event TradeNegotiating(bytes32 indexed tradeId);
-    event TradeAgreed(bytes32 indexed tradeId, uint256 dealValue, bytes32 termsHash);
+    event TradeNegotiating(bytes32 indexed tradeId, bytes32 attestationHash);
+    event TradeAgreed(bytes32 indexed tradeId, uint256 finalDealValue, bytes32 termsHash, bytes32 attestationHash);
     event TradeSettled(bytes32 indexed tradeId, uint256 dealValue, uint256 fee);
     event TradeCancelled(bytes32 indexed tradeId, address indexed initiator);
 }
@@ -28,8 +28,10 @@ interface IBondVault {
     }
 
     function postBondFor(bytes32 tradeId, address party, BondStage stage, uint256 dealValue) external;
+    function escalateBond(bytes32 tradeId, address party, BondStage newStage, uint256 dealValue) external;
     function releaseBonds(bytes32 tradeId, address buyer, address seller) external;
     function slashBond(bytes32 tradeId, address faultyParty, address counterparty) external;
+    function getBondAmount(BondStage stage, uint256 dealValue) external pure returns (uint256);
 }
 
 interface ITreasury {
